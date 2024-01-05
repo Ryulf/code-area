0a4061e2aa32c18c219b12ab462544827e373afb
Matheus Souza <111622706+Ryulf@users.noreply.github.com>
2024-01-05 16:35:27 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>você viveria sua vida mais uma vez e outra e assim eternamente?</h1>
+    return <h1>Você viveria sua vida mais uma vez e outra e assim eternamente?</h1>
 }
 
 export default Home;
